f1f81e7759ea7e3729aaed7b49254b5d5d9c1873
woohyun-jeong <korin57@daum.net>
2020-01-08 01:51:42 +0900

    add broadcast receiver

--- app/src/main/java/com/whjung/broadcasttest/MainActivity.java
+++ app/src/main/java/com/whjung/broadcasttest/MainActivity.java
@@ -1,14 +1,42 @@
 package com.whjung.broadcasttest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AutoPermissions.Companion.loadAllPermissions(this,101);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        AutoPermissions.Companion.parsePermissions(this,requestCode,permissions,this);
+    }
+
+    @Override
+    public void onDenied(int requestCode, @NonNull String[] permissions) {
+        Toast.makeText(this,"permissions denied" + permissions.length, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int requestCode, @NonNull String[] permissions) {
+        Toast.makeText(this,"permissions grate :" + permissions.length,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
